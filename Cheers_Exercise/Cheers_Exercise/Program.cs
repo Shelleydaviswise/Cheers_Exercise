74304acd4b53d5bdec5d0ef0ee249ea1a18e6e75
Shelley Davis-Wise <shelleydaviswise@gmail.com>
2015-10-15 11:15:56 -0500

    Finished Cheer Exercise Part one

--- Cheers_Exercise/Cheers_Exercise/Program.cs
+++ Cheers_Exercise/Cheers_Exercise/Program.cs
@@ -4,12 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Cheers_Exercise
+namespace Cheers
 {
     class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-        }
+        System.Console.WriteLine("Hello there! What's your name?");
+        string name = Console.ReadLine();
+        System.Console.WriteLine("Hi " + name);
+            foreach (char letter in name.ToLower())
+            {
+                if (Char.IsLetter(letter))
+                {
+                    string mnemonic = "halfnorsemix";
+                    string article = "a";
+                    if (mnemonic.Contains(letter))
+                    {
+                        article = "an";
+                    }
+                    System.Console.WriteLine("Give me " + article + "... " + letter);
+                }
+            }
+        System.Console.WriteLine(name.ToUpper() + " is GRAND");
+        System.Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+
+       
+
     }
+}
 }
