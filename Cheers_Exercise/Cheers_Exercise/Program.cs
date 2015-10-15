cd1060f3441b4f49c60bc26ac97c34fa6bfec340
Shelley Davis-Wise <shelleydaviswise@gmail.com>
2015-10-15 13:04:52 -0500

    Finished Birthday Cheers

--- Cheers_Exercise/Cheers_Exercise/Program.cs
+++ Cheers_Exercise/Cheers_Exercise/Program.cs
@@ -12,7 +12,9 @@ namespace Cheers
     {
         System.Console.WriteLine("Hello there! What's your name?");
         string name = Console.ReadLine();
-        System.Console.WriteLine("Hi " + name);
+        System.Console.WriteLine("Hi " + name +" ! When is your birthday? (MM/DD)");
+        string birthday = Console.ReadLine();                
+       
             foreach (char letter in name.ToLower())
             {
                 if (Char.IsLetter(letter))
@@ -23,15 +25,20 @@ namespace Cheers
                     {
                         article = "an";
                     }
-                    System.Console.WriteLine("Give me " + article + "... " + letter);
+                   System.Console.WriteLine("Give me " + article + "... " + letter);  
                 }
             }
-        System.Console.WriteLine(name.ToUpper() + " is GRAND");
+            DateTime today = DateTime.Today;
+            DateTime next = Convert.ToDateTime(birthday);
+
+            if (next < today)
+                next = next.AddYears(1);
+            int numDays = (next - today).Days;
+
+            System.Console.WriteLine(name.ToUpper() + " is GRAND");
+        System.Console.WriteLine("Your birthday is " + numDays + " until your birthday!");
         System.Console.WriteLine("Press any key to exit.");
         Console.ReadKey();
-
-       
-
     }
 }
 }
